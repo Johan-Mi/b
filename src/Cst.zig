6f8de7831bc6de773e4e8b5c6ead50c96dd8c5bb
Johan-Mi <johanmi@protonmail.com>
2024-09-28 19:30:05 +0200

    Indent CST debug output

--- src/Cst.zig
+++ src/Cst.zig
@@ -1,6 +1,10 @@
 const SyntaxKind = @import("Lexer.zig").SyntaxKind;
 const log = @import("std").log.scoped(.cst);
 
+indent: usize = 0,
+
+const indent_width = 2;
+
 pub const Node = struct {};
 
 pub const Token = struct {};
@@ -10,17 +14,16 @@ pub fn init() @This() {
 }
 
 pub fn startNode(self: *@This(), kind: SyntaxKind) void {
-    _ = self; // autofix
-    log.debug("start node: {s}", .{@tagName(kind)});
+    log.debug("{s:[2]}start node: {s}", .{ "", @tagName(kind), self.indent * indent_width });
+    self.indent += 1;
 }
 
 pub fn finishNode(self: *@This()) void {
-    _ = self; // autofix
-    log.debug("finish node", .{});
+    log.debug("{s:[1]}finish node", .{ "", self.indent * indent_width });
+    self.indent -= 1;
 }
 
 pub fn token(self: *@This(), kind: SyntaxKind, text: []const u8) void {
     _ = text; // autofix
-    _ = self; // autofix
-    log.debug("token: {s}", .{@tagName(kind)});
+    log.debug("{s:[2]}token: {s}", .{ "", @tagName(kind), self.indent * indent_width });
 }
