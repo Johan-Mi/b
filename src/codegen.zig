fe008f6cdd94b1920e876e7844c76bfb9ba2fb33
Johan-Mi <johanmi@protonmail.com>
2024-11-19 14:31:34 +0100

    Use decl literals in codegen

--- src/codegen.zig
+++ src/codegen.zig
@@ -2,10 +2,10 @@ const builtin = @import("builtin");
 const llvm = @import("llvm_.zig");
 
 pub fn compile(output_path: [*:0]const u8) !void {
-    const context = llvm.Context.init();
+    const context: *llvm.Context = .init();
     defer context.deinit();
 
-    const module = llvm.Module.init(context);
+    const module: *llvm.Module = .init(context);
     defer module.deinit();
     module.setTarget("x86_64-unknown-linux-gnu");
 
